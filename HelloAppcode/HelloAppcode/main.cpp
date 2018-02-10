3a41c55349ea846f3320fdf5f4fc8f0cb69646d5
Ouyancheng <1024842937@qq.com>
2018-02-10 16:12:58 -0700

    Wow, it passed compiling on clang...

--- HelloAppcode/HelloAppcode/main.cpp
+++ HelloAppcode/HelloAppcode/main.cpp
@@ -8,6 +8,7 @@
 
 #include <cstdio>
 #include <memory>
+#include <functional>
 class ExampleClass {
 public:
     ExampleClass() {}
@@ -17,36 +18,97 @@ public:
     void print() {
         printf("This is an example class\n");
     }
+    template <typename T1, typename T2>
+    auto universalAdd(T1 &t1, T2 &t2) {
+        return t1 + t2;
+    };
 };
 template <typename T, typename ...Args>
 inline auto callFunction(T func, Args&&... args) {
     return func(args...);
 }
-template <typename F, typename C>
+template <typename T>
 struct BoundMemberFunc {
-    BoundMemberFunc(F f, C &c) : f(f), c(c) {}
-    template <typename ...Args>
-    auto operator ()(Args&&... args) {
-        return (c.*f)(args...);
+    BoundMemberFunc() = delete;
+};
+template <typename T, typename C, typename ...Args>
+struct BoundMemberFunc<T(C::*)(Args...)> {
+    BoundMemberFunc(T(C::*func)(Args...), C &c) : func(func), c(c) {}
+    // BoundMemberFunc(const BoundMemberFunc<T(C::*)(Args...)> &bmf) : func(bmf.func), c(bmf.c) {}
+    template <typename ...FArgs>
+    auto operator ()(FArgs&&... args) {
+        return (c.*func)(args...);
     }
-    F f;
+    T(C::*func)(Args...);
     C &c;
 };
-template <typename F, typename C>
-BoundMemberFunc<F, C> bindMemberFunc(F f, C &c) {
-    return BoundMemberFunc<F, C>(f, c);
+template <typename T, typename C, typename ...Args>
+BoundMemberFunc<T(C::*)(Args...)> bindMemberFunc(T(C::*func)(Args...), C &c) {
+    return BoundMemberFunc<T(C::*)(Args...)>(func, c);
 }
 int add(int a, int b) {
     return a + b;
 }
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    // std::cout << "Hello, World!\n";
+template <typename T1>
+T1 nochange(T1 &a) {
+    return a;
+}
+template <typename T>
+class FuncWrapper {
+public:
+    FuncWrapper() = delete;
+};
+template <typename T, typename ...Args>
+class FuncWrapper<T(Args...)> {
+public:
+    FuncWrapper(T (fn)(Args...)) : func(fn) {}
+    auto operator ()(Args&&... args) {
+        return func(args...);
+    }
+private:
+    T (*func)(Args...);
+};
+template <typename C, typename T, typename ...Args>
+class FuncWrapper<BoundMemberFunc<T(C::*)(Args...)>> {
+public:
+    FuncWrapper(BoundMemberFunc<T(C::*)(Args...)> bmf) : func(bmf) {}
+    auto operator ()(Args&&... args) {
+        return func(args...);
+    }
+private:
+    BoundMemberFunc<T(C::*)(Args...)> func;
+};
+template <typename T, typename ...Args>
+FuncWrapper<T(Args...)> makeFuncWrapper(T (func)(Args...)) {
+    return FuncWrapper<T(Args...)>(func);
+}
+template <typename T, typename C, typename ...Args>
+FuncWrapper<BoundMemberFunc<T(C::*)(Args...)>> makeFuncWrapper(BoundMemberFunc<T(C::*)(Args...)> bmf) {
+    return FuncWrapper<BoundMemberFunc<T(C::*)(Args...)>>(bmf);
+}
+void testFuncWrapper() {
+    ExampleClass exampleClass2;
+    auto fw = makeFuncWrapper(add);
+    printf("%d\n", fw(1, 2));
+    auto fw2 = makeFuncWrapper(bindMemberFunc(&ExampleClass::func, exampleClass2));
+    printf("%d\n", fw2(1, 2));
+    auto fw3 = makeFuncWrapper(bindMemberFunc(&ExampleClass::print, exampleClass2));
+    fw3();
+    auto fw4 = makeFuncWrapper(nochange<int>);
+    int a = 1024;
+    printf("%d\n", fw4(a));
+    auto fw5 = makeFuncWrapper(bindMemberFunc(&ExampleClass::universalAdd<int, int>, exampleClass2));
+    int b = 128, c = 128;
+    printf("%d\n", fw5(b, c));
+}
+void testCallFunction() {
     ExampleClass exampleClass;
     auto uptrec = std::make_unique<ExampleClass>();
     printf("%d\n", callFunction(add, 1, 2));
     printf("%lf\n", callFunction([](auto a, auto b) {return a * b;}, 4, 4.3));
     printf("%d\n", callFunction(bindMemberFunc(&ExampleClass::func, exampleClass), 1, 2));
+    callFunction(bindMemberFunc(&ExampleClass::print, exampleClass));
+    printf("%f\n", callFunction(bindMemberFunc(&ExampleClass::universalAdd<float, float>, exampleClass), 4.0f, 4.3f));
     // auto example_class_func = &ExampleClass::func;
     // printf("%d\n", (exampleClass.*example_class_func)(1, 2));
     auto bmf = bindMemberFunc(&ExampleClass::func, exampleClass);
@@ -62,7 +124,12 @@ int main(int argc, const char * argv[]) {
     delete ptrbmf;
     uptrbmf.reset();
     uptrec->print();
-
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // std::cout << "Hello, World!\n";
+    testCallFunction();
+    testFuncWrapper();
     return 0;
 
 }
